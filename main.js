749684d84550fa90fe643ced552014ac455498c2
unknown <aidanbell0182@gmail.com>
2019-08-23 11:54:51 -0400

    start of day, Friday 23rd

--- main.js
+++ main.js
@@ -21,13 +21,15 @@ document.getElementById("defaultOpen").click();
 //      -things to hide/reveal tabs until checkpoints
 
 var speed = {
-  gather: 10,
+  gather: 100,
   shakeTree: 100,
 }
 
 var gameData = {
   nuts: 0,
-
+  goldenNuts: 0,
+  upgradeCost: 100,
+  speed: 100,
 }
 
 var gatherData = {
@@ -36,19 +38,45 @@ var gatherData = {
   level: 0,
   nutsTimeCost: 100,
   nutsTimeLevel: 1,
+  upgraded: false,
 }
 
-//var mainGameLoop = window.setInterval(function() {
-//  getNuts()
-//}, 100)
+var mainGameLoop = window.setInterval(function() {
+  if (gatherData.upgraded === true) {
+    gather();
+    //document.getElementById('gatherLoader')  make it spin
+  }
+}, gameData.speed)
 
 function getNuts() {
   setTimeout(gather, (speed.gather * 100));
 }
+
+function plusHundred() {
+  gameData.nuts += 100;
+  document.getElementById('nutsTotal').innerHTML = Math.round(gameData.nuts) + " Total Nuts";
+}
+
+// GATHER BUTTON
+
 function gather() {
     Math.round(gameData.nuts += gatherData.perClick);
     document.getElementById('nutsTotal').innerHTML = Math.round(gameData.nuts) + " Total Nuts";
 }
+
+function firstGather() {
+  if (gameData.nuts >= gameData.upgradeCost) {
+    Math.round(gameData.nuts -= gameData.upgradeCost);
+    gatherData.upgraded = true;
+    document.getElementById('nutsTotal').innerHTML = Math.round(gameData.nuts) + " Total Nuts";
+    document.getElementById('gatherBuy').innerHTML = "BUY";
+    document.getElementById('gatherCost').innerHTML = Math.round(gatherData.buyCost);
+    document.getElementsByClassName('gatherBuy').removeAttribute("onclick", "firstGather()");
+    document.getElementsByClassName('gatherBuy').onclick = function() {buyGather();};
+    document.getElementById('gatherCost').innerHTML = Math.round(gatherData.buyCost);
+  }
+}
+
 function buyGather() {
   if (gameData.nuts >= gatherData.buyCost) {
     if (gatherData.level > 0 && gatherData.level % 10 === 0){
@@ -61,20 +89,21 @@ function buyGather() {
       document.getElementById('gatherCost').innerHTML = Math.round(gatherData.buyCost);
       document.getElementById('gatherEffect').innerHTML = Math.round(gatherData.perClick);
     } else {
-    Math.round(gameData.nuts -= gatherData.buyCost);
-    Math.round(gatherData.perClick += 1);
-    Math.round(gatherData.buyCost *= 1.25);
-    gatherData.level++;
-    document.getElementById('nutsTotal').innerHTML = Math.round(gameData.nuts) + " Total Nuts";
-    document.getElementById('gatherLevel').innerHTML = gatherData.level;
-    document.getElementById('gatherCost').innerHTML = Math.round(gatherData.buyCost);
-    document.getElementById('gatherEffect').innerHTML = Math.round(gatherData.perClick);
+      Math.round(gameData.nuts -= gatherData.buyCost);
+      Math.round(gatherData.perClick += 1);
+      Math.round(gatherData.buyCost *= 1.25);
+      gatherData.level++;
+      document.getElementById('nutsTotal').innerHTML = Math.round(gameData.nuts) + " Total Nuts";
+      document.getElementById('gatherLevel').innerHTML = gatherData.level;
+      document.getElementById('gatherCost').innerHTML = Math.round(gatherData.buyCost);
+      document.getElementById('gatherEffect').innerHTML = Math.round(gatherData.perClick);
+    }
   }
 }
-}
+
+
 
 function disableClick() {
-
 }
 
 function getNutsBulk() {
