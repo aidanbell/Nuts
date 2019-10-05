d24f1f1ea24931e4132bfaad8172c30eef0969a1
aidanbell0 <aidanbell0182@gmail.com>
2019-10-05 15:39:16 -0400

    Add Science! Add Button class

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
+// JS for page tabs
+
 const nav = document.querySelector('nav')
-
-console.log(nav)
-
 
 let foo = (evt) => {
   document.querySelector('.tab-content.active').classList.replace('active', 'hidden');
@@ -18,14 +17,65 @@ nav.addEventListener('click', foo)
 
 /*----- constants -----*/
 
+class Button {
+  constructor(field, effects, cost) {
+    this.field = field;
+    this.effects = effects;
+    this.cost = cost;
+    this.value = 1;
+    this.multi = 1;
+  }
+};
+
 /*----- app's state (variables) -----*/
-let nutsTotal;
+let nutsTotal = 0;
 
 let getButton = {
   value: 1,
   mult: 1
 }
 
+
 /*----- cached element references -----*/
-/*----- event listeners -----*/
+let TOTAL = document.querySelector('#total');
+
+const homeTab = document.querySelector('#home-content');
+const scienceTab = document.querySelector('#science-content');
+
 /*----- functions -----*/
+const show = () => {
+  if (getValButton) return;
+
+  switch (nutsTotal) {
+    case 50:
+      let upGetButton = new Button('science', 'getVal', 50);
+      break;
+  }
+};
+
+
+const homeHandle = (evt) => {
+  if (evt.target.id === 'get-nuts') {
+    console.log('nuts');
+    getNuts()
+  };
+};
+
+const scienceHandle = (evt) => {
+  if (evt.target.id === 'get-val') {
+    console.log(this);
+    if (nutsTotal < upGetButton.cost) return;
+    getButton.value += upGetButton.value;
+    nutsTotal -= upGetButton.cost;
+    TOTAL.textContent = nutsTotal;
+    upGetButton.cost = Math.floor(upGetButton.cost * 1.1);
+  }
+}
+
+const getNuts = () => {
+  nutsTotal += (getButton.value * getButton.mult);
+  TOTAL.textContent = nutsTotal;
+};
+/*----- event listeners -----*/
+homeTab.addEventListener('click', homeHandle);
+scienceTab.addEventListener('click', scienceHandle);
