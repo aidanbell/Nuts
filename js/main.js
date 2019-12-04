6287274c09196797118a01d80b55ab13fbd2480a
aidanbell0 <aidanbell0182@gmail.com>
2019-12-04 12:12:12 -0500

    Working on Main Game Loop. Transfered Project to react, see https://github.com/aidanbell/Nuts2.0

--- js/main.js
+++ js/main.js
@@ -17,27 +17,51 @@ nav.addEventListener('click', foo)
 
 /*----- constants -----*/
 
+// When you instantiate a class, push it to the appropriate
+// array: timerFunctions
+
 class Button {
-  constructor(field, effects, cost) {
+  constructor(field, effects, cost, time = 1000) {
     this.field = field;
     this.effects = effects;
     this.cost = cost;
+    this.time = time;
     this.value = 1;
     this.multi = 1;
-  };
+  }
 };
+
+class WorkerButton {
+  constructor(time, cost) {
+    this.time = time;
+    this.cost = cost;
+    this.value = 1;
+    this.multi = 1;
+    this.running = false;
+  }
+
+  work = () => {
+    nutsTotal += (this.value * this.multi);
+  }
+}
 
 /*----- app's state (variables) -----*/
 let nutsTotal = 0;
+
+// For mainGameLoop
+
+let timerFunctions = [];
+let secretFunctions = [];
 
 let getButton = {
   value: 1,
   mult: 1
 }
 
-let mainGameLoop = window.setInterval(function() {
 
-}, 1000)
+// const loopFunctions = ()
+
+timerFunctions.forEach(button => button.doThing());
 
 
 /*----- cached element references -----*/
@@ -57,12 +81,22 @@ const show = () => {
   }
 };
 
+const render = () => {
+  TOTAL.textContent = nutsTotal;
+}
 
 
 const homeHandle = (evt) => {
   if (evt.target.id === 'get-nuts') {
     getNuts()
   };
+  if (evt.target.id === 'farm') {
+    if (typeof farmButton === 'undefined') {
+      farmButton = new WorkerButton(1000, 100);
+      farmButton.running = true;
+    }
+    timeout(farmButton);
+  }
 };
 
 const scienceHandle = (evt) => {
@@ -85,6 +119,15 @@ const getNuts = () => {
   nutsTotal += (getButton.value * getButton.mult);
   TOTAL.textContent = nutsTotal;
 };
+
+const timeout = (worker) => {
+  console.log(worker);
+  setTimeout(function() {
+    worker.work();
+    render();
+    timeout();
+  }, time)
+}
 /*----- event listeners -----*/
 homeTab.addEventListener('click', homeHandle);
 scienceTab.addEventListener('click', scienceHandle);
